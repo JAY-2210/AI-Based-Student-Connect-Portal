e4188d7d9661d28ed970fd3f366d640708148656
JAY-2210 <jaydeepgovind.chaudhari@cognizant.com>
2026-05-03 21:55:34 +0530

    create Spring Boot project

---
+++ src/main/java/org/project/studentconnect/StudentConnectApplication.java
@@ -0,0 +1,13 @@
+package org.project.studentconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/studentconnect/StudentConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.studentconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
